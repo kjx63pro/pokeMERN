f3a640e817bb362d2336b2b138a331818a84078c
kjx63 <ueyama.kenji.63@gmail.com>
2020-11-01 11:12:10 +0900

    5. React-Bootstrap Setup, Header & Footer Components

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container, Col } from 'react-bootstrap';
+
+const Footer = () => {
+  return (
+    <footer>
+      <Container>
+        <Col className='text-center'>Copyright &copy; proMERN</Col>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
