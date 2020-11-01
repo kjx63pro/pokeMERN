4e1a4befd61371ef95228cd4d74db6e9fbfe874d
kjx63pro <kjx63pro@gmail.com>
2020-11-01 21:55:19 +0900

    Add collapseOnSelect

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,7 +5,7 @@ import { Navbar, Nav, Container } from 'react-bootstrap';
 const Header = () => {
   return (
     <header>
-      <Navbar bg='success' variant='dark' expand='lg'>
+      <Navbar bg='success' variant='dark' expand='lg' collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
             <Navbar.Brand>PokeSHOP</Navbar.Brand>
