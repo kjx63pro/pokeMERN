4d6399bb5019d1793533d5e4b5d38dcb5fd14a65
kjx63pro <kjx63pro@gmail.com>
2020-11-04 10:20:17 +0900

    37. User Authentication Endpoint

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 import pokemonRoutes from './routes/pokemonRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -12,11 +13,14 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
 app.use('/api/pokemons', pokemonRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 
