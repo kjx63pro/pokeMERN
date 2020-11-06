3ee28c592ce357336e51e045483f29e660297394
kjx63pro <kjx63pro@gmail.com>
2020-11-06 09:43:54 +0900

    43&44 Fronend User Login Funcitonality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import Footer from './components/Footer';
 import HomeScreen from './screens/HomeScreen';
 import PokemonScreen from './screens/PokemonScreen';
 import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
+          <Route path='/login' component={LoginScreen} exact />
           <Route path='/' component={HomeScreen} exact />
           <Route path='/pokemon/:id' component={PokemonScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
