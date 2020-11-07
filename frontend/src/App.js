51566e7feee746c5fbc8f0a8f88584edc2c9c308
kjx63pro <kjx63pro@gmail.com>
2020-11-07 21:02:24 +0900

    48. Profile Screen & Get User Details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import PokemonScreen from './screens/PokemonScreen';
 import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import ProfileScreen from './screens/ProfileScreen';
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
         <Container>
           <Route path='/login' component={LoginScreen} />
           <Route path='/register' component={RegisterScreen} />
+          <Route path='/profile' component={ProfileScreen} />
           <Route path='/pokemon/:id' component={PokemonScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/' component={HomeScreen} exact />
